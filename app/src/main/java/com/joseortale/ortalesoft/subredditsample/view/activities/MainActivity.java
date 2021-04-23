cff7db9b673b8e5851925bf17267ee5f2b2e7266
Jose Ortale <jose.ortale@pzbp.co.uk>
2021-04-23 03:00:14 +0100

    MainActivity moved to activities package

---
+++ app/src/main/java/com/joseortale/ortalesoft/subredditsample/view/activities/MainActivity.java
@@ -0,0 +1,40 @@
+package com.joseortale.ortalesoft.subredditsample.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.joseortale.ortalesoft.subredditsample.R;
+import com.joseortale.ortalesoft.subredditsample.view.fragments.SubmissionsFragment;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SubmissionsFragment fragment = SubmissionsFragment.newInstance();
+        setFragment(fragment);
+    }
+
+    public void setFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.root_fragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        int fragments = getSupportFragmentManager().getBackStackEntryCount();
+        if (fragments == 0) {
+            super.onBackPressed();
+        } else  {
+            getFragmentManager().popBackStack();
+        }
+    }
+}
